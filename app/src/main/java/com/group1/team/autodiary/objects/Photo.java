6b5933ab1e418a25a6082ab93bb4f5d9ba5b1325
Jaemin Hong <hjm0901@gmail.com>
2017-01-21 15:27:51 +0900

    improve selfi

---
+++ app/src/main/java/com/group1/team/autodiary/objects/Photo.java
@@ -0,0 +1,51 @@
+package com.group1.team.autodiary.objects;
+
+import android.util.Log;
+
+public class Photo {
+
+    public static final int ANGER = 0, JOY = 1, SORROW = 2, SURPRISE = 3;
+
+    private long mTime;
+    private int mAnger, mJoy, mSurprise, mSorrow;
+
+    public static int faceToInt(String str) {
+        Log.i("cs496test", str);
+        if (str.equals("VERY UNLIKELY"))
+            return 0;
+        else if (str.equals("UNLIKELY"))
+            return 1;
+        else if (str.equals("LIKELY"))
+            return 2;
+        else if (str.equals("VERY LIKELY"))
+            return 3;
+        return -1;
+    }
+
+    public Photo(long time, int anger, int joy, int sorrow, int surprise) {
+        this.mTime = time;
+        this.mAnger = anger;
+        this.mJoy = joy;
+        this.mSurprise = sorrow;
+        this.mSorrow = surprise;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public int getFeeling(int feel) {
+        switch (feel) {
+            case ANGER:
+                return mAnger;
+            case JOY:
+                return mJoy;
+            case SORROW:
+                return mSorrow;
+            case SURPRISE:
+                return mSurprise;
+            default:
+                return 0;
+        }
+    }
+}
