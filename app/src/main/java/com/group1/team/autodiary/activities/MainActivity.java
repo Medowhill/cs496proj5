09857f27c2911f3167de53e236253fd91038aa88
Jaemin Hong <hjm0901@gmail.com>
2017-01-21 14:53:28 +0900

    fix error

--- app/src/main/java/com/group1/team/autodiary/activities/MainActivity.java
+++ app/src/main/java/com/group1/team/autodiary/activities/MainActivity.java
@@ -14,6 +14,7 @@ import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 
 import com.group1.team.autodiary.R;
@@ -97,11 +98,11 @@ public class MainActivity extends AppCompatActivity {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(getApplicationContext()))
                     finish();
                 break;
-            case REQUEST_PACKAGE_USAGE_STATS :
+            case REQUEST_PACKAGE_USAGE_STATS:
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !hasPackageUsageStatsPermission())
                     finish();
                 break;
-            case REQUEST_NOTIFICATION_LISTENER_ENABLE :
+            case REQUEST_NOTIFICATION_LISTENER_ENABLE:
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !hasNotificationListenerPermission())
                     finish();
                 break;
