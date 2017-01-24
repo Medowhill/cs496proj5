d67714aed298bca0e9ae94c145b2e2a43bf9fe58
Jaemin Hong <hjm0901@gmail.com>
2017-01-24 23:30:35 +0900

    played music, visited place dialog

--- app/src/main/java/com/group1/team/autodiary/Adapters/PlayedMusicStatisticsAdapter.java
+++ app/src/main/java/com/group1/team/autodiary/Adapters/PlayedMusicStatisticsAdapter.java
@@ -5,42 +5,72 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.group1.team.autodiary.R;
-import com.group1.team.autodiary.objects.AppUsage;
 import com.group1.team.autodiary.objects.Music;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-/**
- * Created by q on 2017-01-24.
- */
 
-public class PlayedMusicStatisticsAdapter extends ArrayAdapter<Music> {
-    Context mContext;
-    List<Music> musics;
-    LayoutInflater inflater;
+public class PlayedMusicStatisticsAdapter extends ArrayAdapter<Map.Entry<String, Integer>> {
+
+    private Context mContext;
+    private List<Map.Entry<String, Integer>> musics;
+    private LayoutInflater inflater;
 
     public PlayedMusicStatisticsAdapter(Context context, int resource, List<Music> musics) {
         super(context, resource);
         mContext = context;
-        this.musics = musics;
         inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        for (Music music : musics) {
+            String key = music.getTrack();
+            if (hashMap.containsKey(key))
+                hashMap.put(key, hashMap.get(key) + 1);
+            else
+                hashMap.put(key, 1);
+        }
+
+        this.musics = new ArrayList<>(hashMap.entrySet());
+        Collections.sort(this.musics, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        });
     }
 
     @Override
-    public int getCount() { return musics.size(); }
+    public int getCount() {
+        return musics.size();
+    }
 
     @Override
-    public Music getItem(int position) { return musics.get(position); }
+    public Map.Entry<String, Integer> getItem(int position) {
+        return musics.get(position);
+    }
 
     @Override
-    public long getItemId(int position) { return position; }
+    public long getItemId(int position) {
+        return position;
+    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup viewGroup) {
         if (convertView == null)
             convertView = inflater.inflate(R.layout.statistics_played_music_item, viewGroup, false);
+
+        Map.Entry<String, Integer> entry = musics.get(position);
+        ((TextView) convertView.findViewById(R.id.played_music_item_name)).setText(entry.getKey());
+        ((TextView) convertView.findViewById(R.id.played_music_item_time))
+                .setText(String.format(mContext.getString(R.string.played_musics_format), entry.getValue()));
 
         return convertView;
     }
--- app/src/main/java/com/group1/team/autodiary/Adapters/VisitedPlaceStatisticsAdapter.java
+++ app/src/main/java/com/group1/team/autodiary/Adapters/VisitedPlaceStatisticsAdapter.java
@@ -8,36 +8,44 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.group1.team.autodiary.R;
-import com.group1.team.autodiary.objects.AppUsage;
 import com.group1.team.autodiary.objects.Place;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
-/**
- * Created by q on 2017-01-24.
- */
 
 public class VisitedPlaceStatisticsAdapter extends ArrayAdapter<Place> {
-    Context mContext;
-    List<Place> places;
-    LayoutInflater inflater;
+
+    private Context mContext;
+    private List<Place> places;
+    private LayoutInflater inflater;
+
+    private SimpleDateFormat mFormat;
 
     public VisitedPlaceStatisticsAdapter(Context context, int resource, List<Place> places) {
         super(context, resource);
         mContext = context;
         this.places = places;
         inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mFormat = new SimpleDateFormat(context.getString(R.string.visited_places_time_format), Locale.KOREA);
     }
 
     @Override
-    public int getCount() { return places.size(); }
+    public int getCount() {
+        return places.size();
+    }
 
     @Override
-    public Place getItem(int position) { return places.get(position); }
+    public Place getItem(int position) {
+        return places.get(position);
+    }
 
     @Override
-    public long getItemId(int position) { return position; }
+    public long getItemId(int position) {
+        return position;
+    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup viewGroup) {
@@ -45,7 +53,8 @@ public class VisitedPlaceStatisticsAdapter extends ArrayAdapter<Place> {
             convertView = inflater.inflate(R.layout.statistics_visited_place_item, viewGroup, false);
 
         Place place = places.get(position);
-        ((TextView) convertView.findViewById(R.id.visited_place_item_place)).setText(new Date(place.getTime()).toString() + " - " + new Date(place.getTime() + place.getDuration()).toString());
+        ((TextView) convertView.findViewById(R.id.visited_place_item_time)).setText(mFormat.format(new Date(place.getTime()))
+                + " - " + mFormat.format(new Date(place.getTime() + place.getDuration())));
         ((TextView) convertView.findViewById(R.id.visited_place_item_place)).setText(place.getName());
 
         return convertView;
